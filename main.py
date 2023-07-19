7741dc03b72e2fb773dd1e2fafd4b307b3204521
irenandko <haha_helppls@bk.ru>
2023-07-19 18:01:31 +0600

    мною была создана функция проверки слова на палиндром путем сравнения исходного слова, введенного пользователем, и этого же слова при обратном срезе.  +html/css

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 # 4.1 GIT (начало проекта)
 
-# пример оптимизации кода без вложенных циклов, соответственно, ассимптотическая сложность кода гораздо меньше и не приведет к виду n**2, если все символы уникальны
-# s = 'aab'
-# syms_count = {}
-# for syms in s:
-#     syms_count[syms]=syms_count.get(syms,0)+1
+
+# def palindrome(slovo):
+#     if slovo==slovo[::-1]: return True
+#     else: return False
 #
-# print(syms_count)
-
-# git init - инициализация проекта
-# git status - чек несохраненных файлов (красные не сохранены)
-# git add .   - добавить все, или прописать нужные
-# git commit   - сохранение изменений + коммент
+# slovo = input('Введите слово для проверки на пaлиндром: ').lower()
+# print(palindrome(slovo))
 
 
-def palindrome(slovo):
-    if slovo==slovo[::-1]: return True
-    else: return False
 
-slovo = input('Введите слово для проверки на пaлиндром: ').lower()
-print(palindrome(slovo))
-
+# 4.2 HTML & CSS
 
 
 
