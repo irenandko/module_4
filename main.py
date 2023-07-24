2703cba41c3a278103504fb4b854e1269860f429
irenandko <haha_helppls@bk.ru>
2023-07-24 23:19:26 +0600

    задание для 3 урока на второй странице сайта

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # 4.1 GIT (начало проекта)
 
 
-# def palindrome(slovo):
-#     if slovo==slovo[::-1]: return True
-#     else: return False
-#
-# slovo = input('Введите слово для проверки на пaлиндром: ').lower()
-# print(palindrome(slovo))
+def palindrome(slovo):
+    if slovo==slovo[::-1]: return True
+    else: return False
+
+slovo = input('Введите слово для проверки на пaлиндром: ').lower()
+print(palindrome(slovo))
 
 
 
