fd82a54ba54e1b67e8627aebe2ab507d141ec77c
irenandko <haha_helppls@bk.ru>
2023-08-01 00:24:18 +0600

    7

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок',max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена',max_digits=10,decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Укажите, уместен ли торг')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'advertisements'
+
+    def __str__(self):
+        return  ('id='+ str(self.id) + ', '+ 'title='+ self.title+', '+'price='+ str(self.price))
